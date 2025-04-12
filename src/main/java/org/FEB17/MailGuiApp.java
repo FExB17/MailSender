405b805b0de99977a560ae6b4ef3853a2d4c8960
Furkan <furkanervan@hotmail.de>
2025-04-12 19:10:21 +0200

    added required fields

--- src/main/java/org/FEB17/MailGuiApp.java
+++ src/main/java/org/FEB17/MailGuiApp.java
@@ -1,6 +1,12 @@
 package org.FEB17;
 
+import javax.mail.search.SizeTerm;
 import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MailGuiApp {
 
@@ -16,12 +22,14 @@ public class MailGuiApp {
 
         JTextField mailTo = new JTextField();
         JLabel labelRecipient = new JLabel("Recipient");
+        mailTo.setMaximumSize(new Dimension((Integer.MAX_VALUE),30));
         panel.add(labelRecipient);
         panel.add(mailTo);
 
 
         JTextField subject = new JTextField();
         JLabel labelSubject = new JLabel("Subject");
+        subject.setMaximumSize(new Dimension(Integer.MAX_VALUE,30));
         panel.add(labelSubject);
         panel.add(subject);
 
@@ -35,8 +43,30 @@ public class MailGuiApp {
 
         JButton button = new JButton("Send");
         button.addActionListener(e -> {
+            String to = mailTo.getText();
+            String subj = subject.getText();
+            String message = messageArea.getText();
+
+            Map<String,String> fields = Map.of(
+                    "Recipient",to,
+                    "Subject", subj,
+                    "Message", message
+            );
+
+            List<String> missingFields = new ArrayList<>();
+            for (Map.Entry<String,String> entry: fields.entrySet()){
+                if (entry.getValue().isBlank()){
+                    missingFields.add(entry.getKey());
+                }
+            }
+            if (!missingFields.isEmpty()){
+                String reqFieldsMessage = "Please fill out required fields:\n" + String.join(", ", missingFields);
+                JOptionPane.showMessageDialog(frame, reqFieldsMessage );
+                return;
+            }
+
             JOptionPane.showMessageDialog(frame,"Nachricht wird gesendet");
-            MailSender.sendMail(mailTo.getText(),subject.getText(),messageArea.getText());
+            MailSender.sendMail(to,subj,message);
         });
         panel.add(button);
 
